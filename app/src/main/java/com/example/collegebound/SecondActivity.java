f6eca78c1a111d812b09c91f086cb9736dc72f66
shefalisharma <shefali4@illinois.edu>
2019-12-06 11:42:01 -0600

    College Bound #8

--- app/src/main/java/com/example/collegebound/SecondActivity.java
+++ app/src/main/java/com/example/collegebound/SecondActivity.java
@@ -2,10 +2,12 @@ package com.example.collegebound;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.VibrationEffect;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
@@ -14,6 +16,7 @@ import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
+import android.os.Vibrator;
 import android.widget.ListView;
 
 import java.lang.reflect.Array;
@@ -103,6 +106,36 @@ public class SecondActivity extends AppCompatActivity implements AdapterView.OnI
             return view1;
         }
     }
+
+    Vibrator vib = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+    private void phoneVib(int intensity) {
+        int actualIntensity = 0;
+
+        switch(intensity) {
+            case 1: actualIntensity = 25;
+            case 2: actualIntensity = 50;
+            case 3: actualIntensity = 75;
+            case 4: actualIntensity = 100;
+            case 5: actualIntensity = 125;
+            case 6: actualIntensity = 150;
+            case 7: actualIntensity = 175;
+            case 8: actualIntensity = 200;
+            case 9: actualIntensity = 225;
+            case 10: actualIntensity = 255;
+            default: actualIntensity = 255;
+
+        }
+        long[] pattern = new long[]{0, 100, 1000, 300, 200, 100, 500, 200, 100, 400};
+        int[] amp = new int[]{0, actualIntensity, 0, actualIntensity, 0, actualIntensity, 0, actualIntensity};
+
+        if (vib.hasAmplitudeControl()) {
+            VibrationEffect effect = VibrationEffect.createWaveform(pattern, amp, -1);
+            vib.vibrate(effect);
+        }
+    }
+
+
 
 
 
